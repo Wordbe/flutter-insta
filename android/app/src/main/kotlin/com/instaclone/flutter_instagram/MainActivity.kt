ea086442f6bcdd1c132bd771e106e19ea236629f
Wordbe <seonghojin3@gmail.com>
2023-01-17 00:38:59 +0900

    BottomNavigator

---
+++ android/app/src/main/kotlin/com/instaclone/flutter_instagram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.instaclone.flutter_instagram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/controller/bottom_nav_controller.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_instagram/src/pages/upload.dart';
+import 'package:get/get.dart';
+
+enum PageName {
+  HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE
+}
+
+class BottomNavController extends GetxController {
+  RxInt pageIndex = 0.obs;
+  List<int> bottomHistory = [0];
+
+  void changeBottomNav(int value, {bool hasGesture = true}) {
+    var page = PageName.values[value];
+    switch (page) {
+      case PageName.UPLOAD:
+        Get.to(() => const Upload());
+        break;
+      case PageName.HOME:
+      case PageName.SEARCH:
+      case PageName.ACTIVITY:
+      case PageName.MYPAGE:
+        _changePage(value, hasGesture: hasGesture);
+        break;
+    }
+  }
+
+  void _changePage(int value, {bool hasGesture = true}) {
+    pageIndex(value);
+    if (!hasGesture) return;
+    if (bottomHistory.last != value) {
+      bottomHistory.add(value);
+    }
+  }
+
+  Future<bool> willPopAction() async {
+    if (bottomHistory.length == 1) {
+      return true;
+    } else {
+      bottomHistory.removeLast();
+      changeBottomNav(bottomHistory.last, hasGesture: false);
+      return false;
+    }
+  }
+}
