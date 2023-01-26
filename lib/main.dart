e3e18fce2264ee8656bfd8aa2787c19cc3429550
Wordbe <seonghojin3@gmail.com>
2023-01-27 02:05:10 +0900

    feat: firebase configure, google login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_instagram/src/app.dart';
+import 'package:flutter_instagram/firebase_options.dart';
 import 'package:flutter_instagram/src/binding/init_bindings.dart';
+import 'package:flutter_instagram/src/root.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -22,6 +26,6 @@ class MyApp extends StatelessWidget {
               titleTextStyle: TextStyle(color: Colors.black),
             )),
         initialBinding: InitBinding(),
-        home: const App());
+        home: const Root());
   }
 }
